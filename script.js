fc26e68546a3b5d152fbcba2e4d75277e7f111e6
Miko Peltokorpi <miko.peltokorpi@gmail.com>
2022-03-16 18:37:10 +0200

    Add remove book function

--- script.js
+++ script.js
@@ -6,26 +6,24 @@ function Book(title,author,pages,isBookRead) {
     this.author=author
     this.pages=pages
     this.isBookRead=isBookRead
-    this.info=function() {
-        if (!isBookRead) {
-            let info=`${title} by ${author}, ${pages} pages, not read yet`;
-            return info     
-        }
-        else {
-            let info=`${title} by ${author}, ${pages} pages, read already`;
-            return info       
-        }     
-    }
 }
+
 //function to add books to the array
 function addBookToLibrary(title,author,pages,isBookRead) {
     let newBook=new Book(title,author,pages,isBookRead);
     myLibrary.push(newBook);
+    showFeed();
 }
 
 //function to remove books from the array
 function removeBookFromLibrary(index) {
     myLibrary.splice(index,1);
+    let dataIndex=`[data-index="${index}"]`
+    let removedElem=document.querySelector(dataIndex);
+    removedElem.remove();
+    showFeed();
+
+
 }
 
 //function that creates a div that contains the information
@@ -33,13 +31,14 @@ function showFeed() {
     bookFeed.innerHTML='';
     for (let i=0;i<myLibrary.length;i++) {
         let div=document.createElement('div');
-        div.innerHTML=`<div class="card-body">
+        div.innerHTML=`<div class="card-body" data-index="${i}">
                             <h5 class="card-title">${myLibrary[i].title}</h5>
                             <ul>
                                 <li>Author: ${myLibrary[i].author}</li>
                                 <li>Pages: ${myLibrary[i].pages}</li>
                                 <li>Status: ${(myLibrary[i].isBookRead) ? 'Read' : 'Not read' }</li>
                             </ul>
+                            <button type="button" onclick="removeBookFromLibrary(${i})">Remove book</button>
                         </div>`
         bookFeed.appendChild(div);
     }
@@ -58,6 +57,5 @@ function getFormValues() {
         isBookRead=false;
     }
     addBookToLibrary(title,author,pages,isBookRead)
-
 }
 
