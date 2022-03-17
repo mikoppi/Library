550e9dfefc9e21487ecbde26d75f687d1e904058
Miko Peltokorpi <miko.peltokorpi@gmail.com>
2022-03-17 11:55:46 +0200

    Finish functionality and add skeleton CSS

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let myLibrary=[];
 const bookFeed = document.querySelector('.bookfeed');
 
+//The constructor 
 function Book(title,author,pages,isBookRead) {
     this.title=title
     this.author=author
@@ -22,27 +23,17 @@ function removeBookFromLibrary(index) {
     let removedElem=document.querySelector(dataIndex);
     removedElem.remove();
     showFeed();
-
-
 }
 
-//function that creates a div that contains the information
-function showFeed() {
-    bookFeed.innerHTML='';
-    for (let i=0;i<myLibrary.length;i++) {
-        let div=document.createElement('div');
-        div.innerHTML=`<div class="card-body" data-index="${i}">
-                            <h5 class="card-title">${myLibrary[i].title}</h5>
-                            <ul>
-                                <li>Author: ${myLibrary[i].author}</li>
-                                <li>Pages: ${myLibrary[i].pages}</li>
-                                <li>Status: ${(myLibrary[i].isBookRead) ? 'Read' : 'Not read' }</li>
-                            </ul>
-                            <button type="button" onclick="removeBookFromLibrary(${i})">Remove book</button>
-                        </div>`
-        bookFeed.appendChild(div);
+function changeReadStatus(index) {
+    if (myLibrary[index].isBookRead==true) {
+        myLibrary[index].isBookRead=false;
     }
-}
+    else {
+        myLibrary[index].isBookRead=true;
+    }
+    showFeed();
+}    
 
 //function that takes the values from the form and puts them into object
 function getFormValues() {
@@ -57,5 +48,25 @@ function getFormValues() {
         isBookRead=false;
     }
     addBookToLibrary(title,author,pages,isBookRead)
-}
+}        
+
+
+//function that creates a div that contains the information
+function showFeed() {
+    bookFeed.innerHTML='';
+    for (let i=0;i<myLibrary.length;i++) {
+        let div=document.createElement('div');
+        div.innerHTML=`<div class="card-body" data-index="${i}">
+                            <h5 class="card-title">${myLibrary[i].title}</h5>
+                            <ul>
+                                <li>Author: ${myLibrary[i].author}</li>
+                                <li>Pages: ${myLibrary[i].pages}</li>
+                                <li>Status: ${(myLibrary[i].isBookRead) ? 'Read' : 'Not read' }</li>
+                            </ul>    
+                            <button type="button" onclick="removeBookFromLibrary(${i})">Remove book</button>
+                            <button type="button" onclick="changeReadStatus(${i})">Change status</button>
+                        </div>`    
+        bookFeed.appendChild(div);                
+    }    
+}    
 
